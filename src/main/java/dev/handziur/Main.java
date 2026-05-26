c29ab3df9f484679a23f03560301200eea2204b5
handziurdmytro <handziurdmytro@gmail.com>
2026-05-27 00:03:13 +0300

    feat: add entrypoint

--- src/main/java/dev/handziur/Main.java
+++ src/main/java/dev/handziur/Main.java
@@ -1,7 +1,40 @@
 package dev.handziur;
 
+import dev.handziur.core.Server;
+import dev.handziur.crypto.Crypto;
+import dev.handziur.domain.ConcurrentWarehouse;
+import dev.handziur.network.MockReceiver;
+import dev.handziur.network.MockSender;
+import dev.handziur.protocol.Decoder;
+import dev.handziur.protocol.Encoder;
+import dev.handziur.protocol.Processor;
+
 public class Main {
-    static void main() {
-        System.out.println("sample");
+    public static void main(String[] args) throws Exception {
+        Crypto crypto = new Crypto();
+        Encoder encoder = new Encoder(crypto);
+        Decoder decoder = new Decoder(crypto);
+
+        ConcurrentWarehouse warehouse = new ConcurrentWarehouse();
+        Processor processor = new Processor(warehouse);
+
+        MockReceiver receiver = new MockReceiver(encoder);
+        MockSender sender = new MockSender();
+
+        Server pipeline = new Server(
+                2, 2, 4, 3, 5,
+                receiver, decoder, processor, encoder, sender
+        );
+
+        System.out.println("Starting server pipeline...");
+        pipeline.start();
+
+        Thread.sleep(5000);
+
+        System.out.println("Initiating graceful shutdown...");
+        pipeline.shutdown();
+        System.out.println("Server stopped gracefully.");
+
+        System.out.println("Final 'Mezzanine' Qty: " + warehouse.getItemQty("Mezzanine"));
     }
 }
