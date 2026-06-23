f1d3e814a12d0629df2309925b3617c97715d7c9
handziurdmytro <handziurdmytro@gmail.com>
2026-06-23 22:14:16 +0300

    feat: start http server

--- src/main/java/dev/handziur/Main.java
+++ src/main/java/dev/handziur/Main.java
@@ -1,46 +1,15 @@
 package dev.handziur;
 
-import dev.handziur.core.Server;
-import dev.handziur.crypto.Crypto;
-import dev.handziur.domain.ProductFilterParams;
 import dev.handziur.domain.ProductService;
-import dev.handziur.domain.Product;
-import dev.handziur.network.MockReceiver;
-import dev.handziur.network.MockSender;
-import dev.handziur.protocol.Decoder;
-import dev.handziur.protocol.Encoder;
-import dev.handziur.protocol.Processor;
-
-import java.util.List;
+import dev.handziur.network.http.StoreServerHTTP;
 
 public class Main {
+    private static final short PORT = 2929;
+    private static final String DB_URL = "jdbc:sqlite:store.db";
+
     public static void main(String[] args) throws Exception {
-        Crypto crypto = new Crypto();
-        Encoder encoder = new Encoder(crypto);
-        Decoder decoder = new Decoder(crypto);
-
-        ProductService productService = new ProductService("jdbc:sqlite:warehouse.db");
-        Processor processor = new Processor(productService);
-
-        MockReceiver receiver = new MockReceiver(encoder);
-        MockSender sender = new MockSender();
-
-        Server pipeline = new Server(
-                2, 2, 4, 3, 5,
-                receiver, decoder, processor, encoder, sender
-        );
-
-        System.out.println("Starting server pipeline...");
-        pipeline.start();
-
-        Thread.sleep(5000);
-
-        System.out.println("Initiating graceful shutdown...");
-        pipeline.shutdown();
-        System.out.println("Server stopped gracefully.");
-
-        List<Product> res = productService.search(new ProductFilterParams("Mezzanine", null, null, null, null, null), 1, 0);
-        int finalQty = res.isEmpty() ? 0 : res.get(0).quantity();
-        System.out.println("Final 'Mezzanine' Qty: " + finalQty);
+        ProductService productService = new ProductService(DB_URL);
+        StoreServerHTTP httpServer = new StoreServerHTTP(productService, PORT);
+        httpServer.start();
     }
 }
