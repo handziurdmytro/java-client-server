a9ffe1cd3aedab13a2926c5c026bb894f6a8c016
handziurdmytro <handziurdmytro@gmail.com>
2026-06-23 22:07:45 +0300

    feat(model): add login request and token response records

---
+++ src/main/java/dev/handziur/model/LoginRequest.java
@@ -0,0 +1,3 @@
+package dev.handziur.model;
+
+public record LoginRequest(String username, String password) {}
---
+++ src/main/java/dev/handziur/model/TokenResponse.java
@@ -0,0 +1,3 @@
+package dev.handziur.model;
+
+public record TokenResponse(String token) {}
